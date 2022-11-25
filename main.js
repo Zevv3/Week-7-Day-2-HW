f35ad4039af5774910c142d8d88c3cb565f46c0b
Kelsey Selvaggi <kelsey@Kelseys-MacBook-Pro.local>
2022-11-25 17:43:33 -0500

    finished work

--- main.js
+++ main.js
@@ -21,13 +21,21 @@ const person3 = {
 //     console.log(`${property}: ${person3[property]}`);
 // }
 
-for (const property in person3){
-    if (typeof property[0] == 'object'){
-        for (const property in person3){
-            console.log(`${property}: ${person3[property]}`);
-        }
+// for (let [property, element] of Object.entries(person3)){
+//     if (typeof element == 'object'){
+//         for (property in person3){
+//             console.log(`${property}: ${element}`);
+//         }
+//     }else{
+//         console.log(`${property}: ${element}`);
+//     }
+// }
+
+for (let i = 0; i < Object.keys(person3).length; i++){
+    if (typeof Object.values(person3)[i] == 'object'){
+        console.log(Object.entries(person3)[i]);
     }else{
-        console.log(`${property}: ${person3[property]}`);
+        console.log(Object.values(person3)[i]);
     }
 }
 
@@ -43,7 +51,24 @@ age by 3 years. Use an arrow function for both methods
 
 // Create our Person Prototype
 
+function Person(name, age){
+    this.name = name;
+    this.age = age;
 
+    this.printInfo = () => {
+        console.log(`${this.name} is ${this.age} years old`);
+    }
+
+    this.increaseAge = (increase) => {
+        this.age += increase;
+        console.log(`${this.name} is now ${this.age} years old`);
+    }
+}
+
+let jim = new Person('Jim', 56);
+jim.printInfo()
+jim.increaseAge(1)
+jim.increaseAge(6)
 // Use an arrow to create the printInfo method
 
 // Create another arrow function for the addAge method that takes a single parameter
@@ -53,7 +78,22 @@ age by 3 years. Use an arrow function for both methods
 // =============Exercise #3 ============//
 /*
 
-    Create a Promised based function that will check a string to determine if it's length is greater than 10.
+    Create a Promised based function that will check a string to 
+    determine if it's length is greater than 10.
     If the length is greater than ten console log "Big word". 
-    If the length of the string is less than 10 console log "Small Number"
+    If the length of the string is less than 10 console log
+    "Small Number"
 */
+
+const checkString = (str) => {
+    return new Promise((resolve, reject) => {
+        if (str.length >= 10){
+            resolve(console.log("Big Word"));
+        }else{
+            reject(console.log("Small Number"));
+        }
+    })
+}
+
+checkString('jds')
+checkString('asdfasfearerwerwreew')
